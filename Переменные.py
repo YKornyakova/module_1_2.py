103743953c5e1643f85afe54a15850d5249959ff
Юлия <kornyakova_11@mail.ru>
2024-08-02 20:53:01 +0500

    Add files via upload

--- Переменные.py
+++ Переменные.py
@@ -1,10 +1,10 @@
 # ДЗ урок 2 Переменные
 Course = "Python,"
 print(Course)
-only_tasks = "12,"
-print(only_tasks)
-spend_hour = "1.5,"
-print(spend_hour)
-mean_time_working = "0.125 часа"
-print(mean_time_working)
-print("Курс:", Course, "всего задач:", only_tasks, "затрачено часов:", spend_hour, " среднее время выполнения", mean_time_working )
+only_tasks = "12"
+print(int(only_tasks))
+spend_hour = "1.5"
+print(float(spend_hour))
+spend_hour = "1.5"
+print("Курс:", Course, "всего задач:", only_tasks, "затрачено часов:", spend_hour, " среднее время выполнения", (float(spend_hour)) / (int(only_tasks)))
+

8fb58ae541e1f41570d2b362609cfba61420d27c
Юлия <kornyakova_11@mail.ru>
2024-08-02 21:18:03 +0500

    Add files via upload

--- Переменные.py
+++ Переменные.py
@@ -1,9 +1,9 @@
 # ДЗ урок 2 Переменные
 Course = "Python,"
 print(Course)
-only_tasks = "12"
+only_tasks = 12
 print(int(only_tasks))
-spend_hour = "1.5"
+spend_hour = 1.5
 print(float(spend_hour))
 print("Курс:", Course, "всего задач:", only_tasks, "затрачено часов:", spend_hour, " среднее время выполнения", (float(spend_hour)) / (int(only_tasks)))
 

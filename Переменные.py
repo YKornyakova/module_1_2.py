e360b864a7b6a3005a97928eda2375520d9f3adf
Юлия <kornyakova_11@mail.ru>
2024-08-02 21:29:29 +0500

    Add files via upload

--- Переменные.py
+++ Переменные.py
@@ -5,5 +5,5 @@ only_tasks = 12
 print(int(only_tasks))
 spend_hour = 1.5
 print(float(spend_hour))
-print("Курс:", Course, "всего задач:", only_tasks, "затрачено часов:", spend_hour, " среднее время выполнения", (float(spend_hour)) / (int(only_tasks)))
+print("Курс:", Course, "всего задач:", only_tasks, "затрачено часов:", spend_hour, " среднее время выполнения", (float(spend_hour)) / (int(only_tasks)), "часа")
 
